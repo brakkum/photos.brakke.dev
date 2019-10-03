91c49addd3142fb8de1b2ae5c2ef52b829789295
Daniel Brakke <brakphoto@gmail.com>
2019-10-02 19:52:22 -0500

    photo load transitions

--- src/FileViewer.js
+++ src/FileViewer.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./FileViewer.css";
 
 const FileViewer = ({ dir, file }) => {
 
     const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        setIsLoaded(false);
+    }, [file]);
 
     const fileExt = file.split(".").pop().toLowerCase();
     const imageTypes = ["jpg", "jpeg", "tiff"];
@@ -15,12 +19,10 @@ const FileViewer = ({ dir, file }) => {
             <div className="view-container">
                 {file !== "" && imageTypes.includes(fileExt) &&
                     <img
-                        className={"photo"}
+                        className={"photo " + (isLoaded ? "loaded" : "")}
                         onLoad={() => setIsLoaded(true)}
                         src={`/files/${dir}/${file}`}
-                    >
-
-                    </img>
+                    />
                 }
             </div>
         </div>
