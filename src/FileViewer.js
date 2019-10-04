983ecebb5fa809333259c167ed82d7d8426ca758
Daniel Brakke <brakphoto@gmail.com>
2019-10-03 20:47:23 -0500

    video handling

--- src/FileViewer.js
+++ src/FileViewer.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./FileViewer.css";
 
-const FileViewer = ({ dir, file }) => {
+function FileViewer({ dir, file }) {
 
     const [isLoaded, setIsLoaded] = useState(false);
 
@@ -12,18 +12,32 @@ const FileViewer = ({ dir, file }) => {
     const fileExt = file.split(".").pop().toLowerCase();
     const imageTypes = ["jpg", "jpeg", "tiff"];
     const videoTypes = ["mov", "mp4"];
+    const isPhoto = imageTypes.includes(fileExt);
+    const isVideo = videoTypes.includes(fileExt);
 
     return (
         <div className="file-view">
             {file !== "" && <div className={"loading-div " + (isLoaded ? "loaded" : "")}>Loading...</div>}
             {file !== "" && <div className={"get-link " + (isLoaded ? "loaded" : "")}>Link</div>}
             <div className="view-container">
-                {(file !== "" && imageTypes.includes(fileExt)) ?
+                {isPhoto ?
                     <img
                         className={"photo " + (isLoaded ? "loaded" : "")}
                         onLoad={() => setIsLoaded(true)}
+                        alt={file}
                         src={`/files/${dir}/${file}`}
                     />
+                    :
+                    isVideo ?
+                        <video
+                            className={"video " + (isLoaded ? "loaded" : "")}
+                            key={file}
+                            onLoadedMetadata={() => setIsLoaded(true)}
+                            controls
+                        >
+                            <source src={`/files/${dir}/${file}`} type="video/mp4" />
+                            <source src={`/files/${dir}/${file}`} type="video/mov" />
+                        </video>
                     :
                     <h4>&lt;&lt; Pick Something!</h4>
                 }
